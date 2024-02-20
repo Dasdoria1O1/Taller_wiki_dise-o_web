e7a48fe4382e7f7ce5f73e644a82e6803f31a02a
Dasdoria1O1 <73320019+Dasdoria1O1@users.noreply.github.com>
2024-02-20 18:59:00 -0500

    Add files via upload

---
+++ src/test/java/grupo_2_4/taller/TallerApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo_2_4.taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
