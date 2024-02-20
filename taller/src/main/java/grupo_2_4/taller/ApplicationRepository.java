8e023535c4433a78a3f063b7a64051628b449e2f
Dasdoria1O1 <73320019+Dasdoria1O1@users.noreply.github.com>
2024-02-20 18:49:45 -0500

    Add files via upload

---
+++ taller/src/main/java/grupo_2_4/taller/ApplicationRepository.java
@@ -0,0 +1,11 @@
+package grupo_2_4.taller;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ApplicationRepository extends CrudRepository<Application, Long>{
+    
+}
+
+
+
+
---
+++ taller/src/main/java/grupo_2_4/taller/TallerApplication.java
@@ -0,0 +1,13 @@
+package grupo_2_4.taller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerApplication.class, args);
+	}
+
+}
